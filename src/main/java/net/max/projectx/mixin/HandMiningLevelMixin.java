26c2b8891422da4727881beffa878343d4280473
UprisingMax <97818586+UprisingMax@users.noreply.github.com>
2023-04-28 02:40:47 -0500

    Added More Wood Block Types (PLANKS, DOORS, FENCES, GATES, SLABS, STAIRS, TRAPDOORS)

--- src/main/java/net/max/projectx/mixin/HandMiningLevelMixin.java
+++ src/main/java/net/max/projectx/mixin/HandMiningLevelMixin.java
@@ -13,7 +13,15 @@ public class HandMiningLevelMixin {
     @Inject(method = "canHarvest", at = @At("HEAD"), cancellable = true)
     public void restrictWoodenBlockBreakingByHand(BlockState state, CallbackInfoReturnable<Boolean> cir) {
         // Check if the player is trying to break a wooden block
-        if (state.isIn(BlockTags.LOGS)) {
+        if (state.isIn(BlockTags.LOGS)
+                || state.isIn(BlockTags.PLANKS)
+                || state.isIn(BlockTags.WOODEN_DOORS)
+                || state.isIn(BlockTags.WOODEN_FENCES)
+                || state.isIn(BlockTags.FENCE_GATES)
+                || state.isIn(BlockTags.WOODEN_SLABS)
+                || state.isIn(BlockTags.WOODEN_STAIRS)
+                || state.isIn(BlockTags.WOODEN_TRAPDOORS))
+        {
             // If the player's main hand is empty, prevent breaking the block
             if (((PlayerEntity) (Object) this).getMainHandStack().isEmpty()) {
                 cir.setReturnValue(false);
